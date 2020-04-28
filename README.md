e37d59cdd3bf3064440b69a7159349a80a8a2bc2
StudioCode <ytstudiocode@gmail.com>
2020-04-28 20:48:17 +0200

    Improvements
    - Shortened main method
    - changed commandline argument from "serverjar" to "serverJar"

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 AntiBackdoor is a tool which helps you remove the Minecraft Trolling/Griefing plugin "Backdoor" from your server.
 
 ## How to use AntiBackdoor
-Specify the location of your server jar file and plugins folder with the start arguments `-serverjar` and `-plugins`, then run the program. It will then scan your server jar file and all plugins in the plugin folder and give you the option to delete / disinfect infected files.
+Specify the location of your server jar file and plugins folder with the start arguments `-serverJar` and `-plugins`, then run the program. It will then scan your server jar file and all plugins in the plugin folder and give you the option to delete / disinfect infected files.
--- src/main/java/de/studiocode/antibackdoor/AntiBackdoor.java
+++ src/main/java/de/studiocode/antibackdoor/AntiBackdoor.java
@@ -12,38 +12,22 @@ import java.util.List;
 public class AntiBackdoor {
 
     public static final Logger LOGGER = Logger.getLogger(AntiBackdoor.class);
-
+    
     public static void main(String[] args) throws ParseException, InterruptedException {
         new File("logs/").mkdir();
-
-        Options options = new Options();
-        options.addOption("help", "Show the help menu");
-        options.addOption("serverjar", true, "The path of the server jar file (Spigot / Bukkit)");
-        options.addOption("plugins", true, "The path of the plugins/ folder");
-        options.addOption("usePluginNameCheck", "Activate the plugin name check (can cause false positives)");
-
-        CommandLine cmd = new DefaultParser().parse(options, args);
+        CommandLine cmd = parseArgs(args);
         
-        if (cmd.hasOption("help")) {
-            HelpFormatter help = new HelpFormatter();
-            help.printHelp("java -jar AntiBackdoor.jar -serverjar <FILE> -plugins <FOLDER>", options);
-
-            return;
-        }
-        
-        if (cmd.hasOption("serverjar") || cmd.hasOption("plugins")) {
-            if (!(cmd.hasOption("serverjar") && cmd.hasOption("plugins"))) {
-                LOGGER.warn("Only scanning serverjar or only scanning plugins directory (Continuing in 5s...)");
+        if (cmd.hasOption("serverJar") || cmd.hasOption("plugins")) {
+            if (!(cmd.hasOption("serverJar") && cmd.hasOption("plugins"))) {
+                LOGGER.warn("Only scanning serverJar or only scanning plugins directory (Continuing in 5s...)");
                 Thread.sleep(5000);
             }
-
-            MainScanner searcher = new MainScanner(cmd.hasOption("usePluginNameCheck"));
 
             File serverJar = null;
             File pluginsFolder = null;
 
-            if (cmd.hasOption("serverjar")) {
-                serverJar = new File(cmd.getOptionValue("serverjar"));
+            if (cmd.hasOption("serverJar")) {
+                serverJar = new File(cmd.getOptionValue("serverJar"));
                 if (!serverJar.exists()) LOGGER.fatal("Specified server jar file does not exist.");
             }
 
@@ -52,23 +36,47 @@ public class AntiBackdoor {
                 if (!pluginsFolder.exists() && pluginsFolder.isDirectory())
                     LOGGER.fatal("Specified plugins/ folder does not exist.");
             }
-
-            List<RecommendedAction> recommendedActions = searcher.scanForBackdoor(serverJar, pluginsFolder);
-
-            if (recommendedActions.size() > 0) {
-                LOGGER.warn("There are " + recommendedActions.size() + " recommended actions: ");
-                recommendedActions.forEach(a -> LOGGER.warn(a.getMessage()));
-
-                if (new YesNoQuestion("Do you want to perform them now? (making a backup is recommended)").askQuestion()) {
-                    recommendedActions.forEach(RecommendedAction::performAction);
-                }
-                System.out.println("Goodbye");
-            } else {
-                LOGGER.info("There are no recommended actions. :)");
-            }
-
+            
+            runSearch(serverJar, pluginsFolder, cmd.hasOption("usePluginNameCheck"));
         } else {
             LOGGER.fatal("Nothing to scan provided");
+        }
+    }
+    
+    public static CommandLine parseArgs(String[] args) throws ParseException {
+        Options options = new Options();
+        options.addOption("help", "Show the help menu");
+        options.addOption("serverJar", true, "The path of the server jar file (Spigot / Bukkit)");
+        options.addOption("plugins", true, "The path of the plugins/ folder");
+        options.addOption("usePluginNameCheck", "Activate the plugin name check (can cause false positives)");
+
+        CommandLine commandLine = new DefaultParser().parse(options, args);
+
+        if (commandLine.hasOption("help")) {
+            HelpFormatter help = new HelpFormatter();
+            help.printHelp("java -jar AntiBackdoor.jar -serverJar <FILE> -plugins <FOLDER>", options);
+            
+            System.exit(0);
+        }
+        
+        return commandLine;
+    }
+    
+    public static void runSearch(File serverJar, File pluginsFolder, boolean usePluginNameCheck) {
+        MainScanner searcher = new MainScanner(usePluginNameCheck);
+        
+        List<RecommendedAction> recommendedActions = searcher.scanForBackdoor(serverJar, pluginsFolder);
+
+        if (recommendedActions.size() > 0) {
+            LOGGER.warn("There are " + recommendedActions.size() + " recommended actions: ");
+            recommendedActions.forEach(a -> LOGGER.warn(a.getMessage()));
+
+            if (new YesNoQuestion("Do you want to perform them now? (making a backup is recommended)").askQuestion()) {
+                recommendedActions.forEach(RecommendedAction::performAction);
+            }
+            System.out.println("Goodbye");
+        } else {
+            LOGGER.info("There are no recommended actions. :)");
         }
     }
 
